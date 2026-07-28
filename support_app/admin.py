3bc77f96adecffe1dc373b77576cdec5c17c8212
Nelson Jesus <n.jesus@tracknco.com>
2026-07-28 14:45:02 +0200

    feat(admin): customize user admin interface

--- support_app/admin.py
+++ support_app/admin.py
@@ -1,8 +1,93 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from support_app.models import Contributor, Project, User
 
-# Register your models here to make them accessible via the Django admin interface.
+# Register your models here, making them accessible on Django admin interface.
 admin.site.register(Contributor)
 admin.site.register(Project)
-admin.site.register(User)  # Assuming you have a User model in your support_app.models
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+            (None, {"fields": ("username", "password")}),
+            (
+                    "Informations personnelles",
+                    {
+                            "fields": (
+                                    "birth_date",
+                            )
+                    },
+            ),
+            (
+                    "Consentements",
+                    {
+                            "fields": (
+                                    "can_be_contacted",
+                                    "can_be_contacted_updated_at",
+                                    "can_data_be_shared",
+                                    "can_data_be_shared_updated_at",
+                            )
+                    },
+            ),
+            (
+                    "Permissions",
+                    {
+                            "fields": (
+                                    "is_active",
+                                    "is_staff",
+                                    "is_superuser",
+                                    "groups",
+                                    "user_permissions",
+                            )
+                    },
+            ),
+            (
+                    "Dates importantes",
+                    {
+                            "fields": (
+                                    "last_login",
+                                    "date_joined",
+                            )
+                    },
+            ),
+    )
+
+    readonly_fields = (
+            "can_be_contacted_updated_at",
+            "can_data_be_shared_updated_at",
+            "last_login",
+            "date_joined",
+    )
+
+    add_fieldsets = (
+            (
+                    None,
+                    {
+                            "fields" : (
+                                    "username",
+                                    "password1",
+                                    "password2",
+                                    "birth_date",
+                            ),
+                    },
+            ),
+    )
+
+    list_display = (
+            "username",
+            "is_staff",
+            "is_superuser",
+            "is_active",
+    )
+
+    list_filter = (
+            "is_superuser",
+            "is_staff",
+            "is_active",
+    )
+
+    search_fields = (
+            "username",
+    )
