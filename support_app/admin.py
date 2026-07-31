7c5239286f4ed57287afb9dfa7644a422777eee1
Nelson Jesus <n.jesus@tracknco.com>
2026-07-31 12:20:31 +0200

    feat(admin): enhance user admin interface with additional fields and improved layout

--- support_app/admin.py
+++ support_app/admin.py
@@ -11,86 +11,68 @@ admin.site.register(Project)
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    fieldsets = (
-            (None, {"fields": ("username", "password")}),
-            (
-                    _("Informations personnelles"),
-                    {
-                            "fields": (
-                                    "birth_date",
-                            )
-                    },
-            ),
-            (
-                    _("Consentements"),
-                    {
-                            "fields": (
-                                    "can_be_contacted",
-                                    "can_be_contacted_updated_at",
-                                    "can_data_be_shared",
-                                    "can_data_be_shared_updated_at",
-                            )
-                    },
-            ),
-            (
-                    _("Permissions"),
-                    {
-                            "fields": (
-                                    "is_active",
-                                    "is_staff",
-                                    "is_superuser",
-                                    "groups",
-                                    "user_permissions",
-                            )
-                    },
-            ),
-            (
-                    _("Dates importantes"),
-                    {
-                            "fields": (
-                                    "last_login",
-                                    "date_joined",
-                            )
-                    },
-            ),
+    list_display = ("username", "is_active", "is_staff", "is_superuser")
+    search_fields = ("username",)
+
+    fieldsets = list(UserAdmin.fieldsets)
+    personal_info_title, personal_info_options = fieldsets[1]
+    personal_info_fields = list(personal_info_options["fields"])
+    if "birth_date" not in personal_info_fields:
+        personal_info_fields.append("birth_date")
+    fieldsets[1] = (
+            personal_info_title,
+            {"fields": tuple(personal_info_fields)},
     )
 
-    readonly_fields = (
-            "can_be_contacted",
-            "can_be_contacted_updated_at",
-            "can_data_be_shared",
-            "can_data_be_shared_updated_at",
-            "last_login",
-            "date_joined",
+    fieldsets.append(
+        (
+            _("Consentements"),
+            {
+                "fields": (
+                    "can_be_contacted",
+                    "can_be_contacted_updated_at",
+                    "can_data_be_shared",
+                    "can_data_be_shared_updated_at",
+                ),
+            },
+        )
     )
+    fieldsets = tuple(fieldsets)
+
+    readonly_fields = ("last_login", "date_joined")
 
     add_fieldsets = (
-            (
-                    None,
-                    {
-                            "fields": (
-                                    "username",
-                                    "password1",
-                                    "password2",
-                                    "birth_date",
-                            ),
-                    },
-            ),
+        (
+            _("Informations personnelles"),
+            {
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "birth_date",
+                ),
+            },
+        ),
+        (
+            _("Consentements"),
+            {
+                "fields": (
+                    "can_be_contacted",
+                    "can_data_be_shared",
+                ),
+            },
+        ),
     )
 
-    list_display = (
-            "username",
-            "is_staff",
-            "is_superuser",
-            "is_active",
-    )
-
-    list_filter = (
-            "is_superuser",
-            "is_staff",
-            "is_active",
-    )
-
-    search_fields = (
-            "username",
-    )
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = list(super().get_readonly_fields(request, obj))
+        if obj is not None:
+            readonly_fields.extend(
+                (
+                    "can_be_contacted",
+                    "can_be_contacted_updated_at",
+                    "can_data_be_shared",
+                    "can_data_be_shared_updated_at",
+                )
+            )
+        return readonly_fields
