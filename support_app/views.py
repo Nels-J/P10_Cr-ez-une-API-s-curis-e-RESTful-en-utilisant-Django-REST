36519a858a7d599358d91d6b69a646e99a932604
Nelson Jesus <n.jesus@tracknco.com>
2026-08-14 00:10:47 +0200

    feat: update UserViewSet to use UserSerializer for user creation and improve permission checks

--- support_app/views.py
+++ support_app/views.py
@@ -5,29 +5,34 @@ from rest_framework.request import Request
 from .models import User
 from .serializers import (
     UserDetailSerializer,
-    UserUpdateSerializer,
+    UserUpdateSerializer, UserSerializer,
 )
 
 
 class UserViewSet(viewsets.ModelViewSet[User]):
     permission_classes = [IsAuthenticated]
-    queryset = User.objects.all()
-    lookup_url_kwarg = "user_id"
+    queryset = User.objects.all()  # This is the base queryset for the viewset, which will be filtered based on the action.
+    lookup_url_kwarg = "user_id"  # This is the name of the URL parameter that will be used to look up individual users.
 
     def check_permissions(self, request: Request) -> None:
         if request.method == "POST":
-            return
-        super().check_permissions(request)
+            return  # Allow anyone to create a new user (registration)
+        super().check_permissions(request)  # For other methods, check if the user is authenticated.
 
     def get_serializer_class(
         self,
-    ) -> type[UserDetailSerializer | UserUpdateSerializer]:
+    ) -> type[UserSerializer, UserDetailSerializer | UserUpdateSerializer]:
+        if self.action == "create":
+            return UserSerializer  # For "create" action, use the UserSerializer.
         if self.action in {"update", "partial_update"}:
-            return UserUpdateSerializer
-        return UserDetailSerializer
+            return UserUpdateSerializer  # For "update" and "partial_update" actions, use the UserUpdateSerializer.
+        return UserDetailSerializer  # For "list" and "retrieve" actions, use the UserDetailSerializer.
 
     def get_object(self):
         if self.kwargs.get(self.lookup_url_kwarg) is None:
-            return self.request.user
-        return super().get_object()
+            return self.request.user  # Return the currently authenticated user.
+        return super().get_object()  # If a user_id is provided in the URL, return that user object.
 
+
+# class ProjectViewSet:
+#     pass
