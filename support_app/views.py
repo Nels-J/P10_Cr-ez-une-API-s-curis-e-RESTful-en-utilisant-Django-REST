73c3c49e9bdadb4ed99e2be504a1dcc9b69fe4bc
Nelson Jesus <n.jesus@tracknco.com>
2026-08-13 23:38:09 +0200

    feat: add get_object method to return current user if no lookup key is provided

--- support_app/views.py
+++ support_app/views.py
@@ -3,23 +3,28 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 
 from .models import User
-from .serializers import UserSerializer, UserDetailSerializer
+from .serializers import (
+    UserDetailSerializer,
+    UserUpdateSerializer,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet[User]):
     permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
+    lookup_url_kwarg = "user_id"
 
     def check_permissions(self, request: Request) -> None:
         if request.method == "POST":
             return
         super().check_permissions(request)
 
-    def get_serializer_class(self) -> type[UserSerializer | UserDetailSerializer]:
-        if self.request.method in ["POST"]:
-            return UserSerializer
-        else:
-            return UserDetailSerializer
+    def get_serializer_class(
+        self,
+    ) -> type[UserDetailSerializer | UserUpdateSerializer]:
+        if self.action in {"update", "partial_update"}:
+            return UserUpdateSerializer
+        return UserDetailSerializer
 
     def get_object(self):
         if self.kwargs.get(self.lookup_url_kwarg) is None:
