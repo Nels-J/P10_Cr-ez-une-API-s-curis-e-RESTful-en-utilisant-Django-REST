463bea36401f52d7aa3e5177f0d72634b3e9f1b2
Nelson Jesus <n.jesus@tracknco.com>
2026-08-14 12:30:00 +0200

    feat: enhance admin interface for Contributor and Project models with custom admin classes

--- support_app/admin.py
+++ support_app/admin.py
@@ -5,8 +5,18 @@ from django.utils.translation import gettext_lazy as _
 from support_app.models import Contributor, Project, User
 
 # Register your models here, making them accessible on Django admin interface.
-admin.site.register(Contributor)
-admin.site.register(Project)
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ("project", "contributor", "created_time")
+    search_fields = ("project__name", "contributor__username")
+    list_filter = ("project", "contributor")
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "category", "author")
+    search_fields = ("name", "description", "category", "author__username")
+    list_filter = ("category", "author")
 
 
 @admin.register(User)
